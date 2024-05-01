6a11c1ae9daae321b4131ef6f14deede6c2476bd
Наталья Рём <139891195+NataljaRoehm@users.noreply.github.com>
2024-05-01 17:50:48 +0200

    add Testbase

--- src/test/java/beautybook/UI/TestBase.java
+++ src/test/java/beautybook/UI/TestBase.java
@@ -1,4 +1,25 @@
 package beautybook.UI;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
 public class TestBase {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void init() {
+        driver = new ChromeDriver();
+        driver.get("https://beaty-project-2-0-dwyu.vercel.app/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterMethod(enabled = false)
+    public void terDown() {
+        driver.quit();
+    }
 }
