3449accec146256aac3bb03f81d37817289b0d21
Наталья Рём <139891195+NataljaRoehm@users.noreply.github.com>
2024-05-04 22:33:42 +0200

    change Negative Test

--- src/test/java/beautybook/API/tests/procedure/negativeTests/GetAllProcedureNegativeTest.java
+++ src/test/java/beautybook/API/tests/procedure/negativeTests/GetAllProcedureNegativeTest.java
@@ -1,10 +1,7 @@
 package beautybook.API.tests.procedure.negativeTests;
 
 import beautybook.API.tests.TestBase;
-import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import io.restassured.response.Response;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -17,17 +14,6 @@ public class GetAllProcedureNegativeTest extends TestBase {
                 .when()
                 .get("procedurs")
                 .then()
-                .assertThat().statusCode(401);
-
-    }
-
-    @Test
-    public void verifyProceduresEndpointForForbiddenStatus() {
-        Response response = given()
-                .contentType(ContentType.HTML)
-                .when()
-                .get("procedures");
-
-        Assert.assertEquals(response.getStatusCode(), 403, "Forbidden");
+                .assertThat().statusCode(403);
     }
 }
